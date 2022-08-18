8c90bdfd4d3703051bb5e1e247c66a45d163ff0a
Allie Griffin <afgriffin91@gmail.com>
2022-08-18 10:42:01 -0400

    added environment variables

--- soil_testing_project/settings.py
+++ soil_testing_project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uox1cawff)7@y!q9+&7!d8s6*)pe+r#&sct!s@ksdrip@g+lgj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
