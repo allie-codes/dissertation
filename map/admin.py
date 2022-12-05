c60920b1df0c00d3f250329cc427b193deeba87f
Allie Griffin <afgriffin91@gmail.com>
2022-12-05 09:44:49 -0500

    removed map coordinates from admin site

--- map/admin.py
+++ map/admin.py
@@ -4,15 +4,3 @@ from .models import MapCoordinate
 from geopy.geocoders import Nominatim
 
 # Register your models here.
-class MapCoordinateAdmin(admin.ModelAdmin):
-    list_display = ("participant", "address", "latitude", "longitude",)
-
-    def clean_address(self):
-        address = self.cleaned_data['address']
-        geocoder = Nominatim(user_agent='map')
-        location = geocoder.geocode(address)
-        if not location:
-            raise forms.ValidationError('You must provide a valid address to proceed.')
-        return address
-
-admin.site.register(MapCoordinate, MapCoordinateAdmin)
