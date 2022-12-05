4436cbe14aadbb7929a32df3d1b3f82f07e4c0ec
Allie Griffin <afgriffin91@gmail.com>
2022-12-05 08:07:05 -0500

    added images, polished front end, added tests

--- results/tests.py
+++ results/tests.py
@@ -1,14 +1,18 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from .models import Result
+from participants.models import Participant
 
 # Create your tests here.
 class ResultTests(TestCase):
 
     def setUp(self):
         self.result = Result.objects.create(
-            na = 1.274,
+            sample_label = Participant.objects.create(name='John Smith', email='john@email.com', address = 'Singleton Park, Sketty, Swansea SA2 8PP', sample_1_description = 'back garden', sample_2_description = 'left side garden', sample_3_description = 'right side garden', sample_4_description = 'front garden', sample_5_description = 'vegetable patch', agreement = True),
+            na = '1.274',
+            mg = '0.34',
+            al = '4.36'
         )
     
     def test_result_listing(self):
-        self.assertEqual(f'{self.result.na}', 1.274)
+        self.assertEqual(f'{self.result.na}', '1.274')
