fd44db86ceb4b8d52282453a63c726cf2c7c189b
Allie <93293392+afgriffin@users.noreply.github.com>
2023-04-07 06:20:08 -0400

    Update settings.py

--- soil_testing_project/settings.py
+++ soil_testing_project/settings.py
@@ -89,8 +89,8 @@ AUTHENTICATION_BACKENDS = (
 # 'django.core.mail.backends.console.EmailBackend' 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.sendinblue.com'
-EMAIL_HOST_USER = 'afgriffin91@gmail.com'
-EMAIL_HOST_PASSWORD = 'RZKa0GVzOcjnI3q1'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
