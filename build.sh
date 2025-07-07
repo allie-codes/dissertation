dde10954f61734a107d6462025b07030beeb6b77
Allie <afgriffin91@gmail.com>
2025-07-07 08:44:47 -0400

    Add automatic admin user creation to build script

--- build.sh
+++ build.sh
@@ -9,4 +9,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Run migrations
-python manage.py migrate 
+python manage.py migrate
+
+# Create superuser if it doesn't exist
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser(username='admin', email='admin@ecogardenhealth.com', password='GoodAdminPass123')" | python manage.py shell 
